f05fbb614074517d84754b0544844c469ee887b6
sarfaraznawaz35 <sarfaraznawaz35@gmail.com>
2023-09-04 18:46:42 +0530

    All APIs created for ShiftDetail AB#36

--- src/main/java/com/libraryserver/controller/ShiftDetailController.java
+++ src/main/java/com/libraryserver/controller/ShiftDetailController.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/shiftDetail")
 public class ShiftDetailController {
 
